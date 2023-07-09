94f6fdef9647b33df735b5bdf85a06a436f56629
fares mohalhel <fares.mohalhel@gmail.com>
2023-07-09 23:23:44 +0300

    added payment functionality

---
+++ admin/client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
